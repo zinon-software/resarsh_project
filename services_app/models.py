b5196b71e8fba68072fcad7fcb9971e3074759f8
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2022-11-01 21:26:46 +0200

    One offer for every driver on service

--- services_app/models.py
+++ services_app/models.py
@@ -32,10 +32,12 @@ class Service(models.Model):
 
 class Offer(models.Model):
     driver = models.ForeignKey(Driver, related_name='driver_offer', on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, related_name='service_offer', on_delete=models.CASCADE, unique=True)
+    service = models.ForeignKey(Service, related_name='service_offer', on_delete=models.CASCADE)
     message = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2, default=Decimal(0.00))
     created_dt = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ('driver', 'service')
 
 
 ORDER_STATUE = [
